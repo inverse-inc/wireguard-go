6c62a28f7e8451b7706fa4151825e757b4d608e7
Julien Semaan <jul.semaan@gmail.com>
2020-09-02 13:29:39 -0400

    adjust profile linux file

--- ztn/profile_linux.go
+++ ztn/profile_linux.go
@@ -2,6 +2,14 @@
 
 package ztn
 
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/inverse-inc/packetfence/go/sharedutils"
+	"github.com/inverse-inc/wireguard-go/device"
+)
+
 func (p *Profile) setupInterface(device *device.Device, WGInterface string) {
 	err := exec.Command("ip", "address", "add", "dev", WGInterface, fmt.Sprintf("%s/%d", p.WireguardIP, p.WireguardNetmask)).Run()
 	sharedutils.CheckError(err)
