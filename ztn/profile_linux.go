5f90a567ef29c43e9e13f83e021cf042a04e4c94
Julien Semaan <jul.semaan@gmail.com>
2020-09-03 09:02:47 -0400

    remove hardcoded interface

--- ztn/profile_linux.go
+++ ztn/profile_linux.go
@@ -13,6 +13,6 @@ import (
 func (p *Profile) setupInterface(device *device.Device, WGInterface string) {
 	err := exec.Command("ip", "address", "add", "dev", WGInterface, fmt.Sprintf("%s/%d", p.WireguardIP, p.WireguardNetmask)).Run()
 	sharedutils.CheckError(err)
-	err = exec.Command("ip", "link", "set", "wg0", "up").Run()
+	err = exec.Command("ip", "link", "set", WGInterface, "up").Run()
 	sharedutils.CheckError(err)
 }
