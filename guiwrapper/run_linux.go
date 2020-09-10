63ac72ed8830601153469958452208689aea54ee
Julien Semaan <jul.semaan@gmail.com>
2020-09-10 13:48:21 -0400

    adjust run method for linux

--- guiwrapper/run_linux.go
+++ guiwrapper/run_linux.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
 )
 
 func run() {
-	cmd := exec.Command("sudo", "-E", "../wireguard-go", "-f", "wg0")
+	cmd := exec.Command("sudo", "echo", "Granted root access")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Failed to obtain root access")
+		os.Exit(1)
+	}
+
+	cmd = exec.Command("sudo", "-E", "../wireguard-go", "-f", "wg0")
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
