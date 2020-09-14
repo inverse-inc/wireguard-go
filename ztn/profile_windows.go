d67d2021acb0c39f5ad72c15be180d4793210dc0
Julien Semaan <jul.semaan@gmail.com>
2020-09-14 11:23:50 -0400

    use new error handling to setup windows interface

--- ztn/profile_windows.go
+++ ztn/profile_windows.go
@@ -6,13 +6,11 @@ import (
 	"fmt"
 	"os/exec"
 
-	"github.com/inverse-inc/packetfence/go/sharedutils"
 	"github.com/inverse-inc/wireguard-go/device"
 )
 
-func (p *Profile) setupInterface(device *device.Device, WGInterface string) {
+func (p *Profile) setupInterface(device *device.Device, WGInterface string) error {
 	cmd := exec.Command("netsh", "interface", "ipv4", "set", "address", fmt.Sprintf(`name="%s"`, WGInterface), "static", p.WireguardIP.String(), ipv4MaskString(p.WireguardNetmask))
-	fmt.Println(cmd.String())
 	err := cmd.Run()
-	sharedutils.CheckError(err)
+	return err
 }
