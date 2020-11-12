850b78ebdfc353fd644932f7b234295f842eaeaa
Julien Semaan <jul.semaan@gmail.com>
2020-11-12 08:53:05 -0500

    add routes module for darwin

---
+++ routes/routes_darwin.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+)
+
+func Add(ipnet *net.IPNet, gw net.IP) error {
+	res, err := exec.Command("route", "-n", "add", "-net", ipnet.String(), gw.String()).Output()
+	if err != nil {
+		fmt.Println(string(res))
+	}
+	return err
+}
