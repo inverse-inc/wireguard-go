b3d8b966ee52830b90b33d1a83a877fd0232edd0
Julien Semaan <jul.semaan@gmail.com>
2020-11-13 12:05:41 -0500

    change program files directory name

--- guiwrapper/run_windows.go
+++ guiwrapper/run_windows.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
 
 func run() {
 
-	cmd := exec.Command("C:\\Program Files\\Wireguard\\run.bat", wgenv.Name())
+	cmd := exec.Command("C:\\Program Files\\PacketFence-Zero-Trust-Client\\run.bat", wgenv.Name())
+	fmt.Println(cmd)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Start()
