843488874f13db28abef3d24aa9ce65598e56718
Julien Semaan <jul.semaan@gmail.com>
2020-12-07 10:33:00 -0500

    added missing env constants

---
+++ ztn/env_constants.go
@@ -0,0 +1,24 @@
+package ztn
+
+const (
+	EnvServer          = "WG_SERVER"
+	EnvServerPort      = "WG_SERVER_PORT"
+	EnvServerVerifyTLS = "WG_SERVER_VERIFY_TLS"
+
+	EnvUsername = "WG_USERNAME"
+	EnvPassword = "WG_PASSWORD"
+
+	EnvHonorRoutes = "WG_HONOR_ROUTES"
+
+	EnvBindTechnique = "WG_BIND_TECHNIQUE"
+
+	EnvOffersBridging = "WG_OFFERS_BRIDGING"
+	EnvMaxPeerBridges = "WG_MAX_PEER_BRIDGES"
+
+	EnvGatewayOutboundInterface = "WG_GATEWAY_OUTBOUND_INTERFACE"
+
+	EnvCLIInterractive = "WG_CLI_INTERACTIVE"
+	EnvCLI             = "WG_CLI"
+
+	EnvGUIPID = "WG_GUI_PID"
+)
