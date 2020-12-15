ee75b5783e0520122b2571b21b3426c749435d20
fdurand <fdurand@inverse.ca>
2020-12-15 18:05:44 -0500

    Clean old dns configuration before applying it.

--- main_dns.go
+++ main_dns.go
@@ -118,6 +118,8 @@ func StartDNS() *godnschange.DNSStruct {
 	} else {
 		conf := GenerateCoreDNSConfig(myDNSInfo, profile)
 		CoreDNSConfig = &conf
+		// Clean old modifications
+		dnsChange.RestoreDNS("127.0.0.69")
 		err := dnsChange.Change("127.0.0.69", profile.DomainsToResolve, profile.NamesToResolve, profile.InternalDomainToResolve)
 		if err != nil {
 			dnsChange.Success = false
