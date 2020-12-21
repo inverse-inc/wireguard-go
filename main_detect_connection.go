8e02dcde0ae851a0ef244d95fe7ff593c279840f
Julien Semaan <jul.semaan@gmail.com>
2020-12-21 16:01:22 -0500

    use unix timestamps instead of time.Since

--- main_detect_connection.go
+++ main_detect_connection.go
@@ -9,40 +9,40 @@ import (
 func detectNetworkChange(serverHost string, onchange func()) {
 	go func() {
 		zeroIP := net.IPv4(0, 0, 0, 0)
-		lastFail := time.Time{}
-		lastSuccess := time.Time{}
+		var lastFail int64 = 0
+		var lastSuccess int64 = 0
 		lastIP := zeroIP
-		failTolerance := 60 * time.Second
+		var failTolerance int64 = 60
 		sleepDetectTolerance := failTolerance
 
 		for {
 			time.Sleep(1 * time.Second)
 			conn, err := net.Dial("udp", serverHost)
 			if err != nil {
-				lastSuccess = time.Time{}
+				lastSuccess = 0
 
-				if lastFail.IsZero() {
-					lastFail = time.Now()
-				} else if time.Since(lastFail) > failTolerance {
+				if lastFail == 0 {
+					lastFail = time.Now().Unix()
+				} else if time.Now().Unix()-lastFail >= failTolerance {
 					// Reset the last fail so that if it keeps failing it will only trigger again once failTolerance has elapsed
-					lastFail = time.Time{}
+					lastFail = 0
 					fmt.Println("Too many network failure, triggering network change event")
 					go onchange()
 				}
 			} else {
 				conn.Close()
-				lastFail = time.Time{}
+				lastFail = 0
 				newIP := conn.LocalAddr().(*net.UDPAddr).IP
 
-				if lastSuccess.IsZero() {
-					lastSuccess = time.Now()
-				} else if time.Since(lastSuccess) >= sleepDetectTolerance {
+				if lastSuccess == 0 {
+					lastSuccess = time.Now().Unix()
+				} else if time.Now().Unix()-lastSuccess >= sleepDetectTolerance {
 					// Reset the last success so that it will only trigger again once sleepDetectTolerance has elapsed again
-					lastSuccess = time.Time{}
+					lastSuccess = 0
 					fmt.Println("Last successful connection dates from too long, triggering network change event")
 					go onchange()
 				} else {
-					lastSuccess = time.Now()
+					lastSuccess = time.Now().Unix()
 				}
 
 				if lastIP.IsUnspecified() {
